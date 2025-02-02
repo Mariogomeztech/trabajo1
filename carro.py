8d10c542aa52714673bd4a21ae52a361932e0d75
Mariogomeztech <jormagores@gmail.com>
2025-02-02 16:39:19 -0500

    estaba pendiente por subir el comment de carro.py

--- carro.py
+++ carro.py
@@ -1,11 +1,19 @@
+"""
+carro=("llanta","motor","puerta","silla")
+print (carro)
 carro=("llanta","motor","puerta","silla")
 print (carro)
 
 suma=(2 + 4)
 print (suma)
-
-
+"""
 
 
 barrios=("prado","calazans","comuna13","pachelly")
 print (barrios)
+
+suma=(2 + 4)
+print (suma)
+
+carro=("llanta","motor","puerta","silla")
+print (carro)
